5e4840c0293614f1eb887d12a98eaed7356abbc0
Ilya Butskikh <libut0853@gmail.com>
2021-01-17 20:05:17 +0300

    Car serializer and car creation through admin panel

--- cars/serializers.py
+++ cars/serializers.py
@@ -3,7 +3,17 @@ from rest_framework import serializers
 from .models import Car
 
 class CarSerializer(serializers.ModelSerializer):
-    car_model_name = serializers.RelatedField(source='car_model', read_only=True)
+    car_model_name = serializers.CharField(source='car_model.name', read_only=True)
+    brand_name = serializers.CharField(source='car_model.brand.name', read_only=True)
+    dealer_name = serializers.CharField(source='dealer.name', read_only=True)
+    car_type_name = serializers.CharField(source='car_type.name', read_only=True)
+    body_type_name = serializers.CharField(source='body_type.name', read_only=True)
+    transmission_type_name = serializers.CharField(source='transmission_type.name', read_only=True)
+    engine_type_name = serializers.CharField(source='engine_type.name', read_only=True)
+    
     class Meta:
         model = Car
-        fields = ['id', 'car_model_name', 'dealer', 'pts', 'vin', 'car_type']
+        fields = ['id', 'brand_name', 'car_model_name', 'dealer_name', 'pts', 'vin', 
+            'car_type_name', 'release_year', 'price', 'color', 'mileage', 'wheel',
+            'body_type_name', 'transmission_type_name', 'engine_type_name', 'engine_volume', 'engine_power', 
+            'active']
