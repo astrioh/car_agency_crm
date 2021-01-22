c7abb290d72ef2ddee64336b8f2eabed83729ca3
Ilya Butskikh <libut0853@gmail.com>
2021-01-22 18:58:24 +0300

    Search Component

--- frontend/src/components/SearchInput/SearchInput.jsx
+++ frontend/src/components/SearchInput/SearchInput.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './SearchInput.scss';
 
 const SearchInput = ({ onSearch, className }) => {
   return (
     <div className={'search ' + (className || '')}>
       <input type='text' name='' className='search__input' />
-      <FontAwesomeIcon icon={faSearch} onClick={onSearch} />
+      <FontAwesomeIcon
+        icon={faSearch}
+        onClick={onSearch}
+        style={{ cursor: 'pointer' }}
+      />
     </div>
   );
 };
