f15fe07b3fe3ec0172193b878a6b6178e9fc9051
Ilya Butskikh <libut0853@gmail.com>
2021-01-23 00:29:34 +0300

    Universal Table component

---
+++ frontend/src/components/ObjectTable/ObjectTable.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Table from 'react-bootstrap/Table';
+import './ObjectTable.scss';
+
+const ObjectTable = ({ headings, data, className }) => {
+  return (
+    <Table
+      striped
+      bordered
+      hover
+      className={'object-table ' + (className || '')}
+    >
+      <thead>
+        <tr>
+          {headings.map((heading, i) => (
+            <th key={i}>{heading}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((dataRow, i) => (
+          <tr key={i}>
+            {dataRow.map((dataCol, i) => (
+              <td key={i}>{dataCol}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default ObjectTable;
